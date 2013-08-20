334b637af45f90ef24542037e454b9117719d18a
Nate Lowry <nate.lowry@gmail.com>
2013-08-19 21:59:04 -0500

    test commit

--- Debugger/AssemblyInfo.cs
+++ Debugger/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.CompilerServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 //
-[assembly: AssemblyTitle("")]
+[assembly: AssemblyTitle("Ben's super awesome debugger")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
