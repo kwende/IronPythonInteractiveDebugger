75d4d155cfa93bcef21cb5000529af0d9e211a5f
Ben Rush <ben@ben-rush.net>
2013-08-20 12:07:39 -0500

    re-added the plugin project

--- Debugger/AssemblyInfo.cs
+++ Debugger/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.CompilerServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 //
-[assembly: AssemblyTitle("Ben's super awesome debugger")]
+[assembly: AssemblyTitle("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
